86334c3eb78c513a07f5ef33d2d364fc94a3f79c
Patrice Ferrot <patrice.ferrot@protonmail.com>
2010-07-18 19:37:44 +0000

    Fixed bluewin email issue (the registration link contained "activati_onCode" instead if "activationCode" !?)

--- src/main/java/com/pferrot/lendity/registration/RegistrationConsts.java
+++ src/main/java/com/pferrot/lendity/registration/RegistrationConsts.java
@@ -2,7 +2,7 @@ package com.pferrot.lendity.registration;
 
 public interface RegistrationConsts {
 	
-	String ACTIVATION_CODE_PARAMETER_NAME = "activationCode";
+	String ACTIVATION_CODE_PARAMETER_NAME = "actiCode";
 	String USERNAME_PARAMETER_NAME = "username";
 	String PASSWORD_REGEXP = "[\\p{Graph}]{4,50}";
 }
